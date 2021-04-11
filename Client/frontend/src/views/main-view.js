dc4bf42b05bc052e6edb3cb10477e63981576ef0
evanec <evanec@o2.pl>
2021-04-11 21:29:51 +0200

    fvgdfsa

--- Client/frontend/src/views/main-view.js
+++ Client/frontend/src/views/main-view.js
@@ -5,18 +5,18 @@ class MainView extends PolymerElement {
   static get template() {
     return html`
 <link rel="stylesheet" href="frontend/style.css">
-    <div class="logo">FANTASTYCZNA FAMILIADA</div>
+<div class="logo">FANTASTYCZNA FAMILIADA</div>
 <div class="window">
     <div id="answerSection">
         <div class="question" id="question">Ile waży koń trojański?</div>
-                <div class="textTooltip">
-        <vaadin-text-field id="addAnswerField" class="textInput" theme="medium" placeholder="Wprowadź tutaj odpowiedź do pytania">
-        </vaadin-text-field>
-        <div class="bubble">Zostań jednym z naszych Ankietowanych dodając odpowiedzi na pytania z zakresu szerokopojętej fantastyki, Sciece fiction, filmów, książek, gier, papierowych rpg i tym podobnych.</div>
+        <div class="textTooltip">
+             <vaadin-text-field id="addAnswerField" class="textInput" theme="medium" placeholder="Wprowadź tutaj odpowiedź do pytania">
+            </vaadin-text-field>
+            <div class="bubble">Zostań jednym z naszych Ankietowanych dodając odpowiedzi na pytania z zakresu szerokopojętej fantastyki, Sciece fiction, filmów, książek, gier, papierowych rpg i tym podobnych.</div>
          </div>
         <div class="buttons">
             <vaadin-button theme="" id="saveAnswer" class="button">
-              <iron-icon icon="lumo:plus" slot="prefix"></iron-icon>
+                <iron-icon icon="lumo:plus" slot="prefix"></iron-icon>
                 Dodaj odpowiedź
             </vaadin-button>
             <vaadin-button theme="" id="ignore" class="button middleButton">
@@ -30,22 +30,21 @@ class MainView extends PolymerElement {
         </div>
     </div>
 
-     <hr>
+    <hr>
 
     <div>
-                    <div class="textTooltip">
-         <vaadin-text-field class="textInput" id="addQuestionField" placeholder="Dodaj pytanie"></vaadin-text-field>
-                 <div class="bubbleLeft", id="questionTooltip">Możesz też dodać własną propozycję pytania.</div>
-                  </div>
+        <div class="textTooltip">
+            <div class="bubbleLeft", id="questionTooltip">Możesz też dodać własną propozycję pytania.</div>
+            <vaadin-text-field class="textInput" id="addQuestionField" placeholder="Dodaj pytanie"></vaadin-text-field>
+        </div>
         <div class="buttons">
             <vaadin-button theme="size-l" id="saveQuestion" class="button">
-            <iron-icon icon="lumo:plus" slot="prefix"></iron-icon>
+                <iron-icon icon="lumo:plus" slot="prefix"></iron-icon>
                 Dodaj pytanie
             </vaadin-button>
-                    <div class="bubble">Dzięki za pomoc!</div>
-                     </div>
         </div>
     </div>
+</div>
 </div>
     `;
   }
